334eeadea1c91908c0ba87150f7745b0c1e36a75
Kax675 <kaxozae@gmail.com>
2023-02-09 22:40:28 +0300

    Update +page.svelte

--- src/routes/hospital/+page.svelte
+++ src/routes/hospital/+page.svelte
@@ -57,7 +57,7 @@
 
 <style>
 	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-	@import "/gridjs.theme.css";
+	@import "/gridjs.theme.css?client";
 
 	#hospital-container {
 		display: flex;
