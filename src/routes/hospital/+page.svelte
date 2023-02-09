a575c28754f0fdd29e25810765740b3ba1b95e3d
Seyit <seyyidahmedtopbas@gmail.com>
2023-02-09 18:16:28 +0300

    Update +page.svelte
    klasör

--- src/routes/hospital/+page.svelte
+++ src/routes/hospital/+page.svelte
@@ -57,7 +57,7 @@
 
 <style>
 	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-	@import "/gridjs.theme.css";
+	@import "../lib/gridjs.theme.css";
 
 	#hospital-container {
 		display: flex;
